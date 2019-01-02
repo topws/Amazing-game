fda367e53c668142a334527fdf716a7d753124f9
topws <634794819@qq.com>
2019-01-02 11:46:00 +0800

    1.init project

---
+++ BibleGuardian/BibleGuardian/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BibleGuardian
+//
+//  Created by Avazu Holding on 2019/1/2.
+//  Copyright © 2019 Avazu Holding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
